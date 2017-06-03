7d85ff1b2dc5f06a911594bac8d0b31d207693db
amashty <anvamash@gmail.com>
2017-06-03 23:53:47 +0300

    getting data with interval

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.css";
-import { Navbar, ListGroup, ListGroupItem, Grid, Row, Col } from "react-bootstrap";
-import injectTapEventPlugin from 'react-tap-event-plugin'
-/*
-import { createStore, combineReducers } from 'redux';
-*/
-
-injectTapEventPlugin();
+import { Navbar, ListGroup, ListGroupItem, Grid, Row, Col, ProgressBar } from "react-bootstrap";
 
 
+/*const FIELDSNAME = [
+    {"cloudtotal_pct" : "Amount of total cloud"},
+   { "dewpoint_c":"Dewpoint"},
+    {"feelslike_c":"Feel like temperature"},
+    {"humid_pct":"Humidity level"},
+    {"slp_mb":"Sea level pressure"},
+    {"vis_km":"Visibility"},
+    {"winddir_deg":"Direction wind is coming from"},
+    {"windspd_kts":"Maximum mean wind speed"}]*/
 const FIELDSNAME = [
     "Amount of total cloud",
     "Dewpoint",
@@ -20,8 +23,8 @@ const FIELDSNAME = [
     "Direction wind is coming from",
     "Maximum mean wind speed"]
 
-
-class WeatherLine extends Component {
+//component, showing single line of weather info
+/*class WeatherLine extends React.Component {
     render() {
         return (
             <div>
@@ -34,9 +37,9 @@ class WeatherLine extends Component {
             </div>
         );
     }
-}
-
-class Header extends Component {
+}*/
+//component-header
+class Header extends React.Component {
     render(){
         return(
             <Navbar>
@@ -49,101 +52,99 @@ class Header extends Component {
         )
     }
 }
-class WeatherDisplay extends Component {
+//component for showing loading process if weather==nul, ex.at the start of App
+class WeatherLoading extends React.Component {
+    render(){
+        return(
+            <div>
+                Loading...
+            </div>
+        )
+    }
+}
+class WeatherDisplay extends React.Component {
     constructor() {
         super();
-        this.state = {
+        this.state = {                              //initialize state of component here
             weatherData: null,
-/*
             seconds: 0
-*/
         };
     }
+
+    //Make request to API after component is mounted
+    //Create setInterval to get response/refresh info with an interval of 1 minute
+    //use .bind(this) to be able to use this.setState as setInterval() has it's own/different contex
+    //Use constant url for getting data about weather for today in Tel-Aviv
+
     componentDidMount(){
-        const GEOURL ="http://api-maps.yandex.ru/2.1/?lang=ru_RU";
-        const URL = "http://api.weatherunlocked.com/api/trigger/-7.33,72.42/" +
+        URL = "http://api.weatherunlocked.com/api/trigger/-7.33,72.42/" +
             "current temperature gt 16 includecurrent?" +
             "app_id=30cd5553&app_key=18c2693430a2fc4f7d628166efefa475";
-
-            fetch(URL).then(res => res.json()).then(json => {
-                this.setState({weatherData: json});
-            });
-/*
-        let timer = setInterval(this.tick, 10000)
-*/
-
+        fetch(URL).then(res => res.json()).then(json => {
+            this.setState({weatherData: json})
+        });
+        this.timer = setInterval(this.getData.bind(this), 10000);
     }
-   /* tick() {
-        this.setState({ seconds: this.state.seconds + 1 });
-        console.log(this.state.seconds);
-    }*/
 
+    //Function for getting data from API
+   getData() {
+       fetch(URL).then(res => res.json()).then(json => {
+           this.setState({weatherData: json})
+           }).catch(error => {
+           console.log('error', error)
+       });
+   }
+
+    //Clear interval when components is unmounted/deleted
     componentWillUnmount() {
         clearInterval(this.timer);
     };
 
     render() {
+
         const weatherData = this.state.weatherData;
-        if (!weatherData) return <div> Loading...</div>;
-        const weather = weatherData.CurrentWeather;
-        const iconPath = "img/set/"+weather.wx_icon.replace("gif","png");
-        const date = new Date().toLocaleDateString();
+        /*if (weatherData == null) return (                                                              //
+            <div>
+                <div>Loading...</div>
+                <ProgressBar active now={45} /></div>
+        );*/
+        const weather = weatherData!= null ? weatherData.CurrentWeather : null;
+
+        console.log("render");
+        console.log(weather);
+       /* if(weatherData != null) {
+            const iconPath = "img/set/"+weather.wx_icon.replace("gif","png");
+        }
+
+        const date = new Date().toLocaleDateString();*/
         return (
-            <div className="container">
-                    <Row className="CityAndDate">
-                        <Col md={6} className="City">
-                            <h1> at latitude {weather.lat} and longitude {weather.lon}</h1>
-                        </Col>
-                        <Col md={6} className="Date">
-                            <h1>{date}</h1>
-                        </Col>
-                    </Row>
-
-                    <Row className="WeatherInfo">
-                        <Col md={4}>
-                            <div className="ShortInfo">
-                                <div className="Image"><img src={iconPath} alt={weather.wx_desc}/></div>
-                                <div className="WeatherDesc">{weather.wx_desc}</div>
-                                <div className="Temperature">{weather.temp_c}°</div>
-                            </div>
-                        </Col>
-
-                        <Col md={4} className="GeneralInfo">
-
-                            <ListGroup>
-                                <div>
-                                    { FIELDSNAME.map(function (el, index) {
-                                        return (
-                                            <WeatherLine
-                                                key={index}
-                                                desc={el}/>
-                                                )
-                                        })
-                                    }
-                                </div>
-                            </ListGroup>
-                        </Col>
-                        <Col md={4}>
-                            <div>
-                                <ListGroup className="fields">
-                                    <ListGroupItem> {weather.cloudtotal_pct}%</ListGroupItem>
-                                    <ListGroupItem> {weather.dewpoint_c}°  </ListGroupItem>
-                                    <ListGroupItem> {weather.feelslike_c}°</ListGroupItem>
-                                    <ListGroupItem> {weather.humid_pct}%  </ListGroupItem>
-                                    <ListGroupItem> {weather.slp_in} inches</ListGroupItem>
-                                    <ListGroupItem> {weather.vis_km} kilometers</ListGroupItem>
-                                    <ListGroupItem> {weather.winddir_deg} degrees</ListGroupItem>
-                                    <ListGroupItem> {weather.windspd_kmh} km/h</ListGroupItem>
-                                </ListGroup>
-                            </div>
-                        </Col>
-                    </Row>
+            <div>
+                {Boolean(weather!=null) ?
+                    <div>
+                        <h4> Уже прошло {this.state.seconds} секунд </h4>
+                        <ul className="fields">
+                            <li> {weather.cloudtotal_pct}%</li>
+                            <li> {weather.dewpoint_c}°</li>
+                            <li> {weather.feelslike_c}°</li>
+                            <li> {weather.humid_pct}%</li>
+                            <li> {weather.slp_in} inches</li>
+                            <li> {weather.vis_km} kilometers</li>
+                            <li> {weather.winddir_deg} degrees</li>
+                            <li> {weather.windspd_kmh} km/h</li>
+                        </ul>
+                    </div>
+                    :
+                    <div>
+                        <h4> Уже прошло {this.state.seconds} секунд </h4>
+                        <WeatherLoading />
+                    </div>
+                }
             </div>
         );
     }
 }
 
-class App extends Component {
+class App extends React.Component {
     constructor() {
         super();
         this.state = {
@@ -158,10 +159,5 @@ class App extends Component {
     );
   }
 }
-
-/*
-setInterval(promise(), 1000);
-*/
-
 
 export default App;
